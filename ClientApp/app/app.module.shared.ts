c735f9be1648e07671b5027924fe8500f2230e25
Camiel Luijkx <camiel.luijkx@gmail.com>
2017-09-25 21:46:07 +0200

    added weather component

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { HelloWorldComponent } from './components/helloworld/helloworld.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
     declarations: [
@@ -18,7 +19,8 @@ import { HelloWorldComponent } from './components/helloworld/helloworld.componen
         CounterComponent,
         FetchDataComponent,
         HomeComponent,
-        HelloWorldComponent
+        HelloWorldComponent,
+        WeatherComponent
     ],
     imports: [
         CommonModule,
@@ -30,6 +32,7 @@ import { HelloWorldComponent } from './components/helloworld/helloworld.componen
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: 'hello', component: HelloWorldComponent },
+            { path: 'weather', component: WeatherComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clu.dotnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("[action]/{city}")]
+        public IActionResult City(string city)
+        {
+            return Ok(new { Temp = "27", Summary = "Sunny", City = city });
+        }
+    }
+}
